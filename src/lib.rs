d8ece8471fbe9b4280929285685ed3b7e5b0600f
Simon Walker <s.r.walker101@googlemail.com>
2024-11-01 10:43:49 +0000

    Invoke pytest function from Rust

--- src/lib.rs
+++ src/lib.rs
@@ -2,13 +2,17 @@ use pyo3::prelude::*;
 
 /// Formats the sum of two numbers as string.
 #[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
+fn run_pytest() -> PyResult<()> {
+    Python::with_gil(|py| {
+        let pytest = py.import_bound("pytest")?;
+        pytest.call_method0("console_main")?;
+        Ok(())
+    })
 }
 
 /// A Python module implemented in Rust.
 #[pymodule]
 fn rytest(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_function(wrap_pyfunction!(run_pytest, m)?)?;
     Ok(())
 }
